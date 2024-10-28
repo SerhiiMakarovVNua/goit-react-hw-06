4c006e453a0121c6e51f4317f91005a4efa57f9f
SerhiiMakarovVNua <serhiimakarov79@gmail.com>
2024-10-28 21:39:38 +0200

    react-hw-06.02

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { BsFillPersonFill, BsFillTelephoneFill } from "react-icons/bs";
+import { deleteContact } from '../../redux/contactsSlice';
 import styles from './Contact.module.css';
 
-const Contact = ({ id, name, number, deleteClick }) => {
+const Contact = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteClick = () => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <div className={styles.contactBloc}>
       <div className={styles.contactItem}>
         <p><span className={styles.icon}><BsFillPersonFill size={20}/></span>{name}</p>    
         <p><span className={styles.icon}><BsFillTelephoneFill /></span>{number}</p>
       </div>
-      <button type='button' onClick={() => deleteClick(id)}>Delete</button>
+      <button type='button' onClick={handleDeleteClick}>Delete</button>
     </div>
   );
 }
